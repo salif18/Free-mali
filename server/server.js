7df78bb44292a1e2379df49feac94a092b95525f
salif18 <salifmoctarkonate@gmail.com>
2024-09-14 02:27:00 +0000

    correction denas spcket

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const server = http.createServer(app)
  const io = socketIO(server,
   { 
   cors:{
-    origin:'http://localhost:3000',
+    origin:'https://free-mali.vercel.app',
     //origin:"http://localhost:3001", 
     methods:['GET','POST']
   }}
