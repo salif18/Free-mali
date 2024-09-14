899a477c3118df56ecb9bf00ddb185736d8182c2
salif18 <salifmoctarkonate@gmail.com>
2024-09-14 09:34:55 +0000

    effacer les commentaaire

--- server/app.js
+++ server/app.js
@@ -16,16 +16,9 @@ const archiveCourierRouter = require('./routes/archiveCourier');
 const recomandationRouter = require('./routes/recomandation');
 const videosRtr = require('./routes/videos');
 const imagesRtr = require('./routes/images')
-//configurations
-app.use(cors(
-  // {
-  //   origin:'https://free-mali.vercel.app',
-  //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  //   allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept'],
-  //   credentials: true // Si vous avez besoin d'envoyer des cookies ou des headers d'autorisation
-  // }
-))
 
+//configurations
+app.use(cors());
 app.use(express.json()) 
 app.use('/images',express.static(path.join(__dirname,'images')))
 app.use('/videos',express.static(path.join(__dirname,'videos')))
