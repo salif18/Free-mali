487a45e5876af409364ff289ed0daaffa04460f1
salif18 <salifmoctarkonate@gmail.com>
2024-09-14 02:03:44 +0000

    modification de cors

--- server/app.js
+++ server/app.js
@@ -17,7 +17,13 @@ const recomandationRouter = require('./routes/recomandation');
 const videosRtr = require('./routes/videos');
 const imagesRtr = require('./routes/images')
 //configurations
-app.use(cors())
+app.use(cors({
+    origin: ['https://free-mali.vercel.app', 'https://https://free-mali.restapi.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept'],
+    credentials: true // Si vous avez besoin d'envoyer des cookies ou des headers d'autorisation
+  }))
+  
 app.use(express.json()) 
 app.use('/images',express.static(path.join(__dirname,'images')))
 app.use('/videos',express.static(path.join(__dirname,'videos')))
