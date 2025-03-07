6abeac79dabb375d7d2e1ea1625e394c9855a6c5
salif18 <salifmoctarkonate@gmail.com>
2025-03-07 23:01:55 +0000

    c

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -90,7 +90,7 @@ exports.login = (req, res, next) => {
           .catch((error) => res.status(500).json({ error }));
       }
     })
-    .catch((error) => res.status(500).json({ error }));
+    .catch((error) => res.status(500).json({ error }))
 };
 
 //pour user de recuperer ses donnees
